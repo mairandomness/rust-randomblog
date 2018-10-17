051d8dd66977ffe4f19ff144273b51ee6d8e6d7a
mairandomness <mairakodama@gmail.com>
2018-10-16 20:09:00 -0400

    datetime is working

--- src/models.rs
+++ src/models.rs
@@ -34,7 +34,7 @@ pub struct Post {
     pub id: i32,
     pub user_id: i32,
     pub title: String,
-    pub date: DateTime<Utc>,
+    pub date: NaiveDateTime,
     pub content: String,
     pub published: bool,
 }
