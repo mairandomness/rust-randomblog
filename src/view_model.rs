c1d27ad4405e9e6ca65a08a6b404f5b9657f6761
mairandomness <mairakodama@gmail.com>
2021-06-08 16:29:44 -0700

    Add support for raw html in the markdown post entry

--- src/view_model.rs
+++ src/view_model.rs
@@ -16,13 +16,15 @@ pub struct PostView {
 
 pub fn post_view(post: &Post) -> PostView {
     let copy = post.content.clone();
+    let mut option = ComrakOptions::default();
+    option.render.unsafe_ = true;
     PostView {
         id: post.id,
         user_id: post.user_id,
         title: (post.title).clone(),
         date: post.date.format("%b %-d %Y").to_string(),
         pubdate: post.date.format("%a, %d %b %Y %T GMT").to_string(),
-        content: markdown_to_html(&(post.content), &ComrakOptions::default()),
+        content: markdown_to_html(&(post.content), &option),
         content_preview: if copy.len() > 250 {copy[0..250].to_string()} else {copy.to_string()},
         published: post.published,
     }
